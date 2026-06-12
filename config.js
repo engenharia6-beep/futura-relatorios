3fa2895a0c819b1c909dc7317f2e6ad5497d8311
engenharia6-beep <engenharia6@futuraeletronicos.com.br>
2026-06-12 13:50:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/SEU_ID_RELATORIOS_AQUI/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz4i-1qMNt_IOdefeZtUNa9RdoQJl8xq-KKBBvmdrPIRBQS2bZhLvzBcyZwQI4fabo7fg/exec',
   APP_VERSION: '1.0.0',
   TIMEOUT_MS: 60000,
 };
